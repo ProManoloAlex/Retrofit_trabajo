0807df6625b389f4c89f48530daeab358d772cdd
ProManoloAlex <manuelalex2020@gmail.com>
2024-04-17 17:18:02 -0600

    Avance_02

--- app/src/main/java/plascencia/manuel/retrofit/MainActivity.kt
+++ app/src/main/java/plascencia/manuel/retrofit/MainActivity.kt
@@ -2,11 +2,10 @@ package plascencia.manuel.retrofit
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.launch
 import plascencia.manuel.retrofit.API.ApiRepository
 
@@ -14,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
 
         lifecycleScope.launch {
