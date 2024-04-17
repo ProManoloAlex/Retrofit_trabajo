3d2ea6b26ebb1f9d3e4cc31c6661425016bce153
ProManoloAlex <manuelalex2020@gmail.com>
2024-04-17 17:04:12 -0600

    Avance_01

--- app/src/main/java/plascencia/manuel/retrofit/ZeldaAdapter.kt
+++ app/src/main/java/plascencia/manuel/retrofit/ZeldaAdapter.kt
@@ -3,6 +3,7 @@ package plascencia.manuel.retrofit
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import plascencia.manuel.retrofit.API.ZeldaItem
 
 class ZeldaAdapter(private val lista: List<ZeldaItem>) : RecyclerView.Adapter<ZeldaHolder>() {
@@ -17,6 +18,11 @@ class ZeldaAdapter(private val lista: List<ZeldaItem>) : RecyclerView.Adapter<Ze
         holder.tvTitulo.text = item.name // Asume que ZeldaItem tiene una propiedad 'nombre'
         holder.tvTipo.text = item.image // Asume que ZeldaItem tiene una propiedad 'tipo'
         // Aquí puedes configurar ivFondo con una imagen usando Glide o Picasso
+        Glide.with(holder.itemView.context)
+            .load(item.image)
+            .into(holder.ivFondo)
+
+
     }
 
     override fun getItemCount(): Int {
